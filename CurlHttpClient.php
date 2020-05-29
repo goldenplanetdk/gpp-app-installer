f4c9d9aed56762596f999788257517ce40c00cd6
Eugene Stepanov <evgeniy@goldenplanet.com>
2020-05-29 17:35:34 +0300

    Fixed Notice: Undefined offset: 2

--- CurlHttpClient.php
+++ CurlHttpClient.php
@@ -74,7 +74,7 @@ class CurlHttpClient implements Client
     {
         $header_lines = preg_split("/\r\n|\n|\r/", $message_headers);
         $headers = array();
-        list(, $headers['http_status_code'], $headers['http_status_message']) = explode(' ', trim(array_shift($header_lines)), 3);
+        list(, $headers['http_status_code'], $headers['http_status_message']) = explode(' ', array_shift($header_lines), 3);
         foreach ($header_lines as $header_line) {
             list($name, $value) = explode(':', $header_line, 2);
             $name = strtolower($name);
